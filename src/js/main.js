fd4d8fc34cce77b50b47ac28ea461bf1a0ec691d
KarolChilimoniuk <webdevkarolchilimoniuk@gmail.com>
2019-08-04 14:34:01 +0200

    modified script

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,24 @@ const addBtn = document.querySelector('.button-add--js');
 const removeBtn = document.querySelector('.button-remove--js');
 let counterSpan = document.querySelector('.span-counter--js'), count = 0;
 
+const svgWater = document.querySelector('.img__water--js');
+
+function colorChange (x) {
+  if (count >= 10) {
+    svgWater.style.fill = '#5500ff';
+  }
+  else {
+    svgWater.style.fill = '#2196F3';
+  }
+}
 
 addBtn.addEventListener('click', (x) => {
+  colorChange(x)
   counterSpan.innerHTML = count += 1;
 })
 
 removeBtn.addEventListener('click', (x) => {
+  colorChange(x)
   if (count == 0) {
     counterSpan.innerHTML = 0;
   } else {
